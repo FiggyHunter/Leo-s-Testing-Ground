8db9bd7191a9682d7bc67091a5ac4b113f88acb9
figgyhunter <leonardo.roi3@gmail.com>
2022-12-05 19:51:18 +0100

    overhauled file structure - implemented first page

---
+++ dev/scripts/loader.js
@@ -0,0 +1,15 @@
+const loaderContainer = document.getElementsByClassName("loader-container")[0];
+document.addEventListener("DOMContentLoaded", hideLoaderWithDelay(2))
+
+function hideLoaderWithDelay(seconds) {
+
+        setTimeout(()=> {
+            loaderContainer.style.opacity = 0;
+        },seconds*1000);
+
+        setTimeout(()=> {
+            loaderContainer.remove();
+        },3000)
+    }
+
+  
