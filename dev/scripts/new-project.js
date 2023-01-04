a82e09025b0530882ba7bbc72b54363524b60d89
figgyhunter <leonardo.roi3@gmail.com>
2023-01-04 18:25:06 +0100

    added interactability to the new-project page

--- dev/scripts/new-project.js
+++ dev/scripts/new-project.js
@@ -1,1 +1,12 @@
 import "../styles/pages/_newProject.scss"
+import { invokeTransitioner } from "./helpers/invokeTransitioner";
+
+let goHomeButton = document.getElementsByClassName("content__nav-home")[0];
+let goInfoButton = document.getElementsByClassName("content__nav-info")[0];
+
+goHomeButton.addEventListener("click", () => {
+ invokeTransitioner("index.html");
+});    
+goInfoButton.addEventListener("click",()=>{
+    invokeTransitioner("about.html")
+} );
