5c2a04ba08e5a999b07905ff307cf3adddd2f978
figgyhunter <leonardo.roi3@gmail.com>
2023-01-05 18:04:54 +0100

    users can now scroll smoothly through projects, and generally navigate easier on phone and PC.

--- dev/scripts/complex.js
+++ dev/scripts/complex.js
@@ -1,9 +1,11 @@
 import { invokeTransitioner } from "./helpers/invokeTransitioner";
 import { fadeOut } from "./helpers/invokeFadeOut";
+import {projects_array} from "./helpers/projectObjects"; 
 
 const loaderContainer = document.getElementsByClassName("loader-container")[0];
 const container = document.querySelector('.complex-container');
 const projects = document.getElementsByClassName('project-cover');
+const pages = document.querySelectorAll('.complex-page');
 const close_options = document.getElementsByClassName("close-btn")[0];
 const open_options = document.getElementsByClassName("right__option")[0];
 const open_about_button = document.getElementsByClassName("right__link")[0];
@@ -12,6 +14,53 @@ const prompt = document.getElementsByClassName("right__prompt")[0];
 const display_options = document.getElementsByClassName("options")[0];
 const display_options_item = document.getElementsByClassName("options__item");
 let display_options_item_array = Array.from(display_options_item);
+let pageCounter = 0;
+let navigationBox = document.querySelectorAll(".complex-foo__navigation-item");
+let project_details = document.getElementsByClassName("project-details");
+let description_logo_container;
+
+
+constructProjectPage(project_details[0], projects_array[0]);
+
+constructProjectPage(project_details[1], projects_array[1]);
+
+constructProjectPage(project_details[2], projects_array[2])
+
+function createTechnologiesUsedPicture(path) {
+  let image_element = document.createElement("img");
+
+  image_element.src = path;
+  image_element.alt = "image of technology used";
+  description_logo_container.appendChild(image_element);
+}
+
+function constructProjectPage(page,object) {
+
+  let project_title = page.querySelector(".description__title");
+  let project_image = page.querySelectorAll(".image-container__image")[0];
+  let project__description = page.querySelectorAll(".description__text-container")[0];
+  let github_link = page.querySelectorAll(".cta__btn")[0];
+  let live_view = page.querySelectorAll(".play")[0];
+  description_logo_container = page.querySelectorAll(".description__logos")[0];
+
+  try {
+    project_title.innerHTML = object.title;
+    project_image.src = object.image;
+    project__description.querySelector("p").innerText = object.description;
+      object.technologiesUsed.forEach((technology) => {
+          console.log(`Creating technology: ${technology}
+                       Source: ${object}
+                       Destination: ${page}`);
+                       console.log(page);
+          createTechnologiesUsedPicture(technology);
+      });
+
+    github_link.href = object.sourceCode;
+    live_view.href = object.liveView;
+  } catch (error) {
+    console.log("Object does not exist!" + error);
+  }
+}
 
 function hideLoaderWithDelay(seconds) {
 
@@ -88,4 +137,76 @@ open_options.addEventListener("click", (e) => {
     setTimeout(() => {
         display_options.style.opacity = "1";
     }, 100);
+});
+
+prompt.addEventListener("click", ()=> {
+  fadeOut(prompt, 1000);
+});
+
+document.addEventListener('wheel', (e)=> {
+  throttle(callback, 100,e);
+});
+
+document.addEventListener('touchstart', (e) => {
+  throttle(callback,100,e);
+  throttlePhone(callback,100,e)
+})
+
+let time = Date.now(); 
+
+function throttle(fn, wait, event = undefined) {
+   // we dismiss every wheel event with deltaY less than 4
+      if (Math.abs(event.deltaY) < 4) return
+      setTimeout(() => {
+        if (((time + wait) - Date.now()) < 0) {
+          fn();
+          time = Date.now();
+          }
+          else return;
+      }, 1500);
+}
+
+function throttlePhone(fn, wait, event = undefined) {
+  // we dismiss every wheel event with deltaY less than 4
+     if (Math.abs(event.deltaY) < 4) return
+     setTimeout(() => {
+       if (((time + wait) - Date.now()) < 0) {
+         fn();
+         time = Date.now();
+         }
+         else return;
+     }, 1500);
+}
+
+function callback() {
+  for (let index = 0; index < pages.length; index++) {
+    onVisible(pages[index], () => {
+      // console.log(pages[index]);
+      for(let j=0; j< navigationBox.length; j++ ) {
+        navigationBox[j].style.backgroundColor = "transparent";
+      }
+      navigationBox[index].style.backgroundColor = "white";
+    })
+  }
+}
+
+function onVisible(element, callback) {
+  new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.intersectionRatio === 1) {
+        callback(element);
+      }
+    });
+  }).observe(element);
+}
+
+
+navigationBox.forEach((box) => {
+  box.addEventListener("click", () => {
+    callback();
+    setTimeout(() => {
+      callback();
+    }, 1000);
+    pages[Array.from(navigationBox).indexOf(box)].scrollIntoView();
+  })
 })
