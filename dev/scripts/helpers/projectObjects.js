e1a9da01ca31de9d134ead9c399e070b61fd33cd
figgyhunter <leonardo.roi3@gmail.com>
2023-01-09 16:30:14 +0100

    updated project descriptions with relevant information.

--- dev/scripts/helpers/projectObjects.js
+++ dev/scripts/helpers/projectObjects.js
@@ -26,16 +26,16 @@ calculator.technologiesUsed.push(img_javascript);
 calculator.technologiesUsed.push( img_html);
 calculator.technologiesUsed.push(img_css);
 
-calculator.description = "This is a description delivered from Javascript.";
-calculator.sourceCode = "https://www.google.ba";
-calculator.liveView = "https://www.indiehackers.com";
+calculator.description = "This is an example of a simple calculator, in which you can perform basic calculations. If you are on PC - You can also use your keyboard to enter values and perform math operations.";
+calculator.sourceCode = "https://github.com/FiggyHunter/SimpleCalculator";
+calculator.liveView = "https://www.leotg.com/calculator";
 
 businessWebsite.technologiesUsed.push(img_javascript);
 businessWebsite.technologiesUsed.push( img_html);
 businessWebsite.technologiesUsed.push(img_css);
 
-businessWebsite.description = "This is my business website!";
-businessWebsite.sourceCode = "https://www.reddit.com";
+businessWebsite.description = "A website built following the premade design in Figma. Some of the website features are a nice interactive menu, classy color pattern and a custom made menu!";
+businessWebsite.sourceCode = "https://github.com/FiggyHunter/BusinessWebsite";
 businessWebsite.liveView = "https://www.leotg.com/businesswebsite"
 
 dadjokes.technologiesUsed.push(img_javascript);
@@ -44,14 +44,8 @@ dadjokes.technologiesUsed.push(img_css);
 dadjokes.technologiesUsed.push(img_axios);
 dadjokes.technologiesUsed.push(img_webpack);
 
-dadjokes.description = "This is a website where you can find some cool jokes!";
-dadjokes.sourceCode = "https://www.discord.com";
+dadjokes.description = "A website that contains various dad jokes, built as an excercise to learning webpack. Even if you don't think about hiring me, you should pay a visit here. It's really funny! ";
+dadjokes.sourceCode = "https://github.com/FiggyHunter/Dad-Jokes";
 dadjokes.liveView = "https://www.leotg.com/dadjokes"
 
 export const projects_array = [calculator,businessWebsite,dadjokes];
-
-
-// let a = [4,5,32,12]
-// let calculator = new Project("Neki", "Image", a)
-
-// console.log(calculator);
