f30f62adecd7495795b237decb5d49e36f349101
figgyhunter <leonardo.roi3@gmail.com>
2023-01-10 13:47:06 +0100

    added new project object, and updated with relevant info

--- dev/scripts/helpers/projectObjects.js
+++ dev/scripts/helpers/projectObjects.js
@@ -1,11 +1,16 @@
 import img_calculator from "../../images/calculator.jpg"
 import img_businessWebsite from "../../images/businesswebsite.png"
 import img_dadJokes from "../../images/dadjokes.png";
+import img_leotg from "../../images/leotg.png";
 import img_javascript from "../../images/javascript.svg";
 import img_html from "../../images/html.svg";
 import img_css from "../../images/css.svg";
 import img_axios from "../../images/axios.svg";
 import img_webpack from "../../images/webpack.svg";
+import img_vite from "../../images/vite.svg";
+import img_penpot from "../../images/penpot.svg";
+import img_sass from "../../images/sass.svg";
+import img_lighthouse from "../../images/lighthouse.svg";
 
 
 class Project {
@@ -21,6 +26,7 @@ class Project {
 let calculator = new Project('Calculator', img_calculator);
 let businessWebsite = new Project ('Business Website', img_businessWebsite);
 let dadjokes = new Project ('Dad Jokes', img_dadJokes);
+let leotg = new Project (`Leo's Testing Ground`,img_leotg);
 
 calculator.technologiesUsed.push(img_javascript);
 calculator.technologiesUsed.push( img_html);
@@ -43,9 +49,22 @@ dadjokes.technologiesUsed.push( img_html);
 dadjokes.technologiesUsed.push(img_css);
 dadjokes.technologiesUsed.push(img_axios);
 dadjokes.technologiesUsed.push(img_webpack);
+dadjokes.technologiesUsed.push(img_sass);
 
 dadjokes.description = "A website that contains various dad jokes, built as an excercise to learning webpack. Even if you don't think about hiring me, you should pay a visit here. It's really funny! ";
 dadjokes.sourceCode = "https://github.com/FiggyHunter/Dad-Jokes";
-dadjokes.liveView = "https://www.leotg.com/dadjokes"
+dadjokes.liveView = "https://www.leotg.com/dadjokes";
 
-export const projects_array = [calculator,businessWebsite,dadjokes];
+leotg.description = "Leo Testing ground is a website made to showcase projects that are made by me, Leonardo Roić. This website's main goal is to present projects to potential clients in an unique way. It's basically a project catalogue that has been overenginered to look as visualy pleasent, as possible."
+leotg.sourceCode = "https://github.com/FiggyHunter/Leo-s-Testing-Ground";
+leotg.liveView = "http://localhost:8080/index-complex.html";
+
+leotg.technologiesUsed.push(img_html);
+leotg.technologiesUsed.push(img_css);
+leotg.technologiesUsed.push(img_javascript);
+leotg.technologiesUsed.push(img_vite);
+leotg.technologiesUsed.push(img_penpot);
+leotg.technologiesUsed.push(img_sass);
+leotg.technologiesUsed.push(img_lighthouse);
+
+export const projects_array = [calculator,businessWebsite,dadjokes,leotg];
