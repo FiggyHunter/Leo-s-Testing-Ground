856e9f96dc8fa4d2c0ddb2b33acd3e579ee7e1ce
figgyhunter <leonardo.roi3@gmail.com>
2023-03-16 09:49:40 +0100

    formatted

--- dev/scripts/helpers/invokeTransitioner.js
+++ dev/scripts/helpers/invokeTransitioner.js
@@ -1,8 +1,10 @@
 export function invokeTransitioner(location) {
-    let transitioner = document.createElement("div");
-    transitioner.id = "transitioner";
-    document.body.insertBefore(transitioner, document.body.firstChild);
-    window.getComputedStyle(transitioner).opacity;
-    setTimeout( transitioner.style.opacity = 1 , 500 );
-    setTimeout( () => { window.location = location }, 2000 );
+  let transitioner = document.createElement("div");
+  transitioner.id = "transitioner";
+  document.body.insertBefore(transitioner, document.body.firstChild);
+  window.getComputedStyle(transitioner).opacity;
+  setTimeout((transitioner.style.opacity = 1), 500);
+  setTimeout(() => {
+    window.location = location;
+  }, 800);
 }
