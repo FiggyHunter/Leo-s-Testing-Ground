7b071e3c2433376d6260cc5f25234ec828de5d0c
figgyhunter <leonardo.roi3@gmail.com>
2023-03-16 09:50:05 +0100

    separated loading logic, so it can be reused as module

---
+++ dev/scripts/helpers/hideLoaderWithDelay.js
@@ -0,0 +1,11 @@
+export default function hideLoaderWithDelay(loaderContainer, seconds) {
+  setTimeout(() => {
+    getComputedStyle(loaderContainer).opacity;
+    loaderContainer.style.opacity = 0;
+    document.title = "Leo's Testing Ground | Home";
+  }, seconds * 1000);
+
+  setTimeout(() => {
+    loaderContainer.remove();
+  }, seconds * 1300);
+}
