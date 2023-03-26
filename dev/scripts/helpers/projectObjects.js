a5a42ffe498dcca3a20eab043e8c1d86457ff16a
figgyhunter <leonardo.roi3@gmail.com>
2023-03-26 16:14:59 +0200

    Modified project Objects so we can render a tooltip

--- dev/scripts/helpers/projectObjects.js
+++ dev/scripts/helpers/projectObjects.js
@@ -12,6 +12,51 @@ import img_penpot from "../../images/penpot.svg";
 import img_sass from "../../images/sass.svg";
 import img_lighthouse from "../../images/lighthouse.svg";
 
+const javascript_technology = {
+  technology: "Javascript",
+  path: img_javascript,
+};
+
+const html_technology = {
+  technology: "HTML",
+  path: img_html,
+};
+
+const css_technology = {
+  technology: "CSS",
+  path: img_css,
+};
+
+const axios_technology = {
+  technology: "Axios",
+  path: img_axios,
+};
+
+const webpack_technology = {
+  technology: "Webpack",
+  path: img_webpack,
+};
+
+const vite_technology = {
+  technology: "Vite",
+  path: img_vite,
+};
+
+const penpot_technology = {
+  technology: "Penpot",
+  path: img_penpot,
+};
+
+const sass_technology = {
+  technology: "Sass",
+  path: img_sass,
+};
+
+const lighthouse_technology = {
+  technology: "Lighthouse",
+  path: img_lighthouse,
+};
+
 class Project {
   constructor(
     title,
@@ -34,30 +79,30 @@ let businessWebsite = new Project("Business Website", img_businessWebsite);
 let dadjokes = new Project("Dad Jokes", img_dadJokes);
 let leotg = new Project(`Leo's Testing Ground`, img_leotg);
 
-calculator.technologiesUsed.push(img_javascript);
-calculator.technologiesUsed.push(img_html);
-calculator.technologiesUsed.push(img_css);
+calculator.technologiesUsed.push(javascript_technology);
+calculator.technologiesUsed.push(html_technology);
+calculator.technologiesUsed.push(css_technology);
 
 calculator.description =
   "This is an example of a simple calculator, in which you can perform basic calculations. If you are on PC - You can also use your keyboard to enter values and perform math operations.";
 calculator.sourceCode = "https://github.com/FiggyHunter/SimpleCalculator";
 calculator.liveView = "https://www.leotg.com/calculator";
 
-businessWebsite.technologiesUsed.push(img_javascript);
-businessWebsite.technologiesUsed.push(img_html);
-businessWebsite.technologiesUsed.push(img_css);
+businessWebsite.technologiesUsed.push(javascript_technology);
+businessWebsite.technologiesUsed.push(html_technology);
+businessWebsite.technologiesUsed.push(css_technology);
 
 businessWebsite.description =
   "A website built following the premade design in Figma. Some of the website features are a nice interactive menu, classy color pattern and a custom made menu!";
 businessWebsite.sourceCode = "https://github.com/FiggyHunter/BusinessWebsite";
 businessWebsite.liveView = "https://www.leotg.com/businesswebsite";
 
-dadjokes.technologiesUsed.push(img_javascript);
-dadjokes.technologiesUsed.push(img_html);
-dadjokes.technologiesUsed.push(img_css);
-dadjokes.technologiesUsed.push(img_axios);
-dadjokes.technologiesUsed.push(img_webpack);
-dadjokes.technologiesUsed.push(img_sass);
+dadjokes.technologiesUsed.push(javascript_technology);
+dadjokes.technologiesUsed.push(html_technology);
+dadjokes.technologiesUsed.push(css_technology);
+dadjokes.technologiesUsed.push(axios_technology);
+dadjokes.technologiesUsed.push(webpack_technology);
+dadjokes.technologiesUsed.push(sass_technology);
 
 dadjokes.description =
   "A website that contains various dad jokes, built as an excercise to learning webpack. Even if you don't think about hiring me, you should pay a visit here. It's really funny! ";
@@ -69,12 +114,12 @@ leotg.description =
 leotg.sourceCode = "https://github.com/FiggyHunter/Leo-s-Testing-Ground";
 leotg.liveView = "";
 
-leotg.technologiesUsed.push(img_html);
-leotg.technologiesUsed.push(img_css);
-leotg.technologiesUsed.push(img_javascript);
-leotg.technologiesUsed.push(img_vite);
-leotg.technologiesUsed.push(img_penpot);
-leotg.technologiesUsed.push(img_sass);
-leotg.technologiesUsed.push(img_lighthouse);
+leotg.technologiesUsed.push(html_technology);
+leotg.technologiesUsed.push(css_technology);
+leotg.technologiesUsed.push(javascript_technology);
+leotg.technologiesUsed.push(vite_technology);
+leotg.technologiesUsed.push(penpot_technology);
+leotg.technologiesUsed.push(sass_technology);
+leotg.technologiesUsed.push(lighthouse_technology);
 
 export const projects_array = [calculator, businessWebsite, dadjokes, leotg];
