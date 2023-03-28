45138f7f0b298dc3d8559aafd4ae6ee3b79dfddc
figgyhunter <leonardo.roi3@gmail.com>
2023-03-28 22:49:37 +0200

    removed unused code

--- dev/scripts/about.js
+++ dev/scripts/about.js
@@ -1,5 +1,4 @@
 import "../styles/pages/_about.scss";
-import { invokeTransitioner } from "./helpers/invokeTransitioner";
 import { ConditionalTransitioner } from "./helpers/invokeConditionalTransitioner";
 
 const aboutMe = document.getElementsByClassName("about-person")[0];
@@ -17,25 +16,8 @@ let itemsArr = Array.from(items);
 let textArr = Array.from(document.getElementsByClassName("main-text"));
 const home__image = document.getElementsByClassName("about-nav__image")[0];
 const home__icon = document.getElementsByClassName("about-nav__image-ctr")[0];
-const nav_home_icon = Array.from(
-  document.getElementsByClassName("nav__image-ctr")
-);
-const nav_home_icon_two = Array.from(
-  document.getElementsByClassName("nav__image-ctr-two")
-);
 
-nav_home_icon.forEach((icon) => {
-  ConditionalTransitioner(icon);
-});
-
-nav_home_icon_two.forEach((icon) => {
-  ConditionalTransitioner(icon);
-});
-
-home__icon.addEventListener("click", (e) => {
-  e.preventDefault();
-  invokeTransitioner("index.html");
-});
+ConditionalTransitioner(home__icon);
 
 function hideText(delay) {
   textArr.forEach((text) => {
