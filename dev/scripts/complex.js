b7caed87d1bb1dba5b55a5a1cdb38a6dd30e7653
figgyhunter <leonardo.roi3@gmail.com>
2023-04-03 16:01:25 +0200

    user now has a smooth dynamic navigation update depending on where he is on the page

--- dev/scripts/complex.js
+++ dev/scripts/complex.js
@@ -1,9 +1,10 @@
-import hideLoaderWithDelay from "./helpers/hideLoaderWithDelay";
+import { hideLoaderWithDelay } from "./helpers/hideLoaderWithDelay";
 import { invokeTransitioner } from "./helpers/invokeTransitioner";
 import { fadeOut } from "./helpers/invokeFadeOut";
 import "../styles/pages/_complexProjects.scss";
 import { renderProjectForComplex } from "./helpers/renderProjectForComplex";
 import { ShowOrHidePrompt } from "./helpers/showOrHidePrompt";
+import { scrollIntoView } from "seamless-scroll-polyfill";
 
 const imgUrl = new URL(
   "../images/calculator-project-image.png",
@@ -15,8 +16,7 @@ const imgUrl3 = new URL("../images/dadjokecover.png", import.meta.url).href;
 const imgUrl4 = new URL("../images/leotgcover.png", import.meta.url).href;
 
 const loaderContainer = document.getElementsByClassName("loader-container")[0];
-const container = document.querySelector(".complex-container");
-const pages = document.querySelectorAll(".complex-page");
+const pages = Array.from(document.querySelectorAll(".complex-page"));
 const close_options = document.getElementsByClassName("close-btn")[0];
 const open_options = document.getElementsByClassName("right__option")[0];
 const prompt = document.getElementsByClassName("right__prompt")[0];
@@ -25,16 +25,64 @@ const display_options_item = document.getElementsByClassName("options__item");
 const project_add = document.getElementsByClassName(
   "projects__project--new"
 )[0];
-const project_close = Array.from(
+const about_me_button = document.getElementsByClassName("right__item-one")[0];
+const description_close = Array.from(
   document.getElementsByClassName("description__close")
 );
-const first_page = document.getElementsByClassName("complex-hero")[0];
-const about_me_button = document.getElementsByClassName("right__item-one")[0];
+
 let display_options_item_array = Array.from(display_options_item);
 let navigationBox = document.querySelectorAll(".complex-foo__navigation-item");
 let project_covers = document.getElementsByClassName("project-cover__darken");
 
+let transitioning = false;
+let currentPage = 0;
+
+const options = {
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+
+const onPage = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      for (let j = 0; j < navigationBox.length; j++) {
+        navigationBox[j].classList.remove("--active-link");
+      }
+      currentPage = pages.indexOf(entry.target);
+      if (currentPage === 0) {
+        navigationBox[0].classList.add("--active-link");
+      }
+      if (currentPage === 2) {
+        navigationBox[1].classList.add("--active-link");
+      }
+      if (currentPage === 4) {
+        navigationBox[2].classList.add("--active-link");
+      }
+      if (currentPage === 6) {
+        navigationBox[3].classList.add("--active-link");
+      }
+      if (currentPage === 8) {
+        navigationBox[4].classList.add("--active-link");
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onPage, options);
+
+pages.forEach((page) => {
+  observer.observe(page);
+});
+
+description_close.forEach((home) => {
+  home.addEventListener("click", () => {
+    pages[0].scrollIntoView();
+    currentPage = 0;
+  });
+});
+
 (function () {
+  hideLoaderWithDelay(loaderContainer, 5);
   window.onpageshow = function (event) {
     if (event.persisted) {
       window.location.reload();
@@ -49,39 +97,41 @@ for (let i = 1; i <= 4; i++) {
 }
 
 window.addEventListener("wheel", (e) => {
+  console.log(currentPage);
   if (e.ctrlKey) return;
-  throttle(callback, 1000, e);
+
+  if (transitioning) return;
+
+  transitioning = true;
+
+  if (e.deltaY > 0) currentPage += 1;
+  else currentPage -= 1;
+
+  if (currentPage < 0) currentPage = 0;
+  if (currentPage > pages.length - 1) currentPage = pages.length - 1;
+  scrollIntoView(
+    pages[currentPage],
+    {
+      behavior: "smooth",
+      block: "start",
+    },
+    {
+      duration: 10, // aprox. the duration that chrome uses,
+    }
+  );
+
+  setTimeout(() => {
+    transitioning = false;
+  }, 200);
 });
 
-window.addEventListener("touchstart", (e) => {
-  throttle(callback, 1000, e);
-  throttlePhone(callback, 1000, e);
-});
+// window.addEventListener("touchstart", (e) => {
+//   throttle(callback, 1000, e);
+//   throttlePhone(callback, 1000, e);
+// });
 
 window.addEventListener("load", () => {
   hideLoaderWithDelay(loaderContainer, 3);
-  project_close.forEach((project) => {
-    project.addEventListener("click", () => {
-      first_page.scrollIntoView();
-      setTimeout(() => {
-        callback();
-      }, 1500);
-    });
-  });
-});
-
-window.addEventListener("wheel", (evt) => {
-  if (evt.ctrlKey) return;
-  //scroll deirection
-  let delta = evt.deltaY;
-  //take widht of container:
-  let contWidth = evt.target.offsetWidth;
-
-  // check direction and apply to contWidth
-  if (delta < 0) {
-    contWidth = -contWidth;
-  }
-  container.scrollLeft += contWidth;
 });
 
 about_me_button.addEventListener("click", (e) => {
@@ -103,15 +153,13 @@ open_options.addEventListener("click", (e) => {
 });
 
 prompt.addEventListener("click", () => {
-  prompt.addEventListener("click", () => {
-    if (
-      sessionStorage.getItem("prompt") === undefined ||
-      sessionStorage.getItem("prompt") === null
-    ) {
-      sessionStorage.setItem("prompt", "closed");
-      fadeOut(prompt, 1000);
-    } else prompt.style.opacity = "0";
-  });
+  if (
+    sessionStorage.getItem("prompt") === undefined ||
+    sessionStorage.getItem("prompt") === null
+  ) {
+    sessionStorage.setItem("prompt", "closed");
+    fadeOut(prompt, 1000);
+  } else prompt.style.opacity = "0";
 });
 
 // scrollintoview js.
@@ -171,35 +219,35 @@ function throttlePhone(fn, wait, event = undefined) {
   }, 1500);
 }
 
-function callback() {
-  for (let index = 0; index < pages.length; index++) {
-    onVisible(pages[index], () => {
-      // console.log(pages[index]);
-      for (let j = 0; j < navigationBox.length; j++) {
-        navigationBox[j].classList.remove("--active-link");
-      }
-      navigationBox[index - 1].classList.add("--active-link");
-    });
-  }
-}
+// function callback() {
+//   for (let index = 0; index < pages.length; index++) {
+//     onVisible(pages[index], () => {
+//       // console.log(pages[index]);
+//       for (let j = 0; j < navigationBox.length; j++) {
+//         navigationBox[j].classList.remove("--active-link");
+//       }
+//       navigationBox[index - 1].classList.add("--active-link");
+//     });
+//   }
+// }
 
-function onVisible(element, callback) {
-  new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.intersectionRatio === 1) {
-        callback(element);
-      }
-    });
-  }).observe(element);
-}
+// function onVisible(element, callback) {
+//   new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.intersectionRatio === 1) {
+//         callback(element);
+//       }
+//     });
+//   }).observe(element);
+// }
 
 navigationBox.forEach((box) => {
   box.addEventListener("click", () => {
-    callback();
-    setTimeout(() => {
-      callback();
-    }, 1000);
-    pages[Array.from(navigationBox).indexOf(box) + 1].scrollIntoView();
+    let index = Array.from(navigationBox).indexOf(box);
+    console.log(index);
+    pages[index + index].scrollIntoView();
+
+    currentPage = index + index;
   });
 });
 
